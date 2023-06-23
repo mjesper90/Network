91d9eedb10ea06454f59b092c8d5eff93c33239d
Vilius Ivanovas <ivanovasvilius@gmail.com>
2023-06-23 16:11:42 +0200

    Implimented ray casting clicks to find which tile to choose

--- Assets/Scripts/Chess/Board.cs
+++ Assets/Scripts/Chess/Board.cs
@@ -6,34 +6,41 @@ namespace Chess
 {
     public class Board : MonoBehaviour
     {
-        public List<Piece> ActivePieces = new List<Piece>();
+        // public List<Piece> ActivePieces = new List<Piece>();
 
         // Start is called before the first frame update
         void Start()
         {
+            
+
             // Spawn in the different pieces using the TilesToVector method
         }
 
         // Update is called once per frame
         void Update()
         {
-
-        }
-
-        public bool SpaceOccupied(int[] Tile)
-        {
-            Vector3 TestPos = TileToVector(Tile);
-
-            foreach (var piece in ActivePieces)
+            if (Input.GetMouseButtonDown(0))
             {
-                if (piece.position = TestPos) 
+                RaycastHit hit;
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit))
                 {
-                    return false;
+                    Vector3 vector3 = hit.point;
+
+                    int[] ints = Vector3ToTile(vector3);
+
+                    print(ints[0] + " " + ints[1]);
                 }
             }
-
-            return true;
         }
+
+        /*public Piece CheckTile()
+        {
+            // Check for if there is a piece on this tile and return said piece
+
+            return null;
+        }*/
 
         public Vector3 TileToVector(int[] TileCoordinate)
         {
@@ -42,7 +49,12 @@ namespace Chess
 
         public int[] Vector3ToTile(Vector3 Position)
         {
-            return int[Math.Round((int)(Position.X / 1.14f), 0), Math.Round((int)(Position.Z/1.14f), 0)];
+            int x = (int)Mathf.Round((Position.x / 1.14f) * 1f);
+            int z = (int)Mathf.Round((Position.z / 1.14f) * 1f);
+
+            int[] Tile = new int[2] { x, z };
+
+            return Tile;
         }
     }
 }
