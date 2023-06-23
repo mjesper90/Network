c792bf93abd6e0f3f25ca8aae542d539378c5be7
Vilius Ivanovas <ivanovasvilius@gmail.com>
2023-06-23 13:44:55 +0200

    Setup the baseline structure for Chess

---
+++ Assets/Scripts/Chess/Pieces.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pieces : MonoBehaviour
+{
+    public virtual List<int[]> PossibleMoves();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Setup all the stuff for that any chess piece would need
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for collisions and such
+    }
+
+    bool Move()
+    {
+        if (ValidMove())
+        {
+            return true; // Shows success in moving the piece
+        }
+        else
+        {
+            return false; // Gives an error, not letting the user initiate such a move
+        }
+    }
+
+    bool ValidMove()
+    {
+        // Check for the piece type initiating script, where to move and so on
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/Chess/Queen.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Queen : Pieces
+{
+    public override List<int[]> PossibleMoves();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
