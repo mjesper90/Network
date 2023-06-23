a8749d008fae53623f662b1d3a5d478196f05a33
Jesper Madsen <m.jesper90@gmail.com>
2023-06-23 13:10:06 +0200

    bump

--- Assets/Scripts/AmongUs/Tasks/MyFirstTask.cs
+++ Assets/Scripts/AmongUs/Tasks/MyFirstTask.cs
@@ -12,6 +12,11 @@ public class MyFirstTask : MonoBehaviour, Interactable
 
     public void OpenTaskWindow()
     {
-        
+        /*
+        GameObject taskWindow = Instantiate(Resources.Load("TaskWindow")) as GameObject;
+        taskWindow.transform.SetParent(GameObject.Find("Canvas").transform);
+        taskWindow.transform.localPosition = new Vector3(0, 0, 0);
+        taskWindow.transform.localScale = new Vector3(1, 1, 1);
+        */
     }
 }
