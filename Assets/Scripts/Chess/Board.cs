86d4638ea2bf9bef556fda5a20fcb0ab975a0fe0
Vilius Ivanovas <ivanovasvilius@gmail.com>
2023-08-08 14:50:23 +0200

    Working on getting it to work, starting spawning pieces

--- Assets/Scripts/Chess/Board.cs
+++ Assets/Scripts/Chess/Board.cs
@@ -1,3 +1,5 @@
+using Chess.Pieces;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,12 +10,14 @@ namespace Chess
     {
         public GameObject Selector;
 
+        public List<GameObject> PiecePrefabs = new List<GameObject>();
+
         public List<Piece> ActivePieces = new List<Piece>();
 
         // Start is called before the first frame update
         void Start()
         {
-            
+            StartPosition(0);
 
             // Spawn in the different pieces using the TilesToVector method
         }
@@ -49,12 +53,12 @@ namespace Chess
             }
         }
 
-        public Vector3 TileToVector(int[] TileCoordinate)
+        public static Vector3 TileToVector(int[] TileCoordinate)
         {
-            return new Vector3((float)TileCoordinate[0] * 1.15f, 0.01f, (float)TileCoordinate[1] * 1.15f);
+            return new Vector3((float)TileCoordinate[0] * 1.15f, 0.2f, (float)TileCoordinate[1] * 1.15f);
         }
 
-        public int[] Vector3ToTile(Vector3 Position)
+        public static int[] Vector3ToTile(Vector3 Position)
         {
             int x = (int)Mathf.Round((Position.x / 1.15f) * 1f);
             int z = (int)Mathf.Round((Position.z / 1.15f) * 1f);
@@ -62,6 +66,39 @@ namespace Chess
             int[] Tile = new int[2] { x, z };
 
             return Tile;
+        }
+
+        public void StartPosition(int owner)
+        {
+            SetupPiece(4, 4, 0, owner);
+
+            for (int i = 0; i < 8; i++)
+            {
+                SetupPiece(0, i, 1, owner);
+            }
+        }
+
+        /* Piece value:
+        0: Pawn
+        1: Rook
+        2: Knight
+        3: Bishop
+        4: Queen
+        5: King */
+        public void SetupPiece(int value, int x, int y, int owner)
+        {
+            GameObject piece = Instantiate(PiecePrefabs[value]);
+            SetLocation(piece, x, y);
+        }
+
+        public static void SetLocation(GameObject piece, int x, int y)
+        {
+            int[] pos = new int[2];
+
+            pos[0] = x;
+            pos[1] = y;
+
+            piece.transform.position = Board.TileToVector(pos);
         }
     }
 }
